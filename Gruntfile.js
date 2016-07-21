f4e7f14ada8c70db7342855ceb2a20438aa3d54d
ahvar <ahvargas92@gmail.com>
2016-07-21 14:28:16 -0400

    Initial Commit
    Create directory structure
    Use npm to install Grunt and configure default settings
    Create gitignore file

---
+++ Gruntfile.js
@@ -0,0 +1,84 @@
+module.exports = function(grunt) {
+
+	var app_files = ['src/fileA.js','src/fileB.js'],
+		output = 'dist/built.js',
+		test_output = 'test/built.js';
+
+	//project configuration
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		copy: {
+			dist: {
+				cwd: 'src/',
+				expand: true,
+				src: '**',
+				dest: 'dist/'
+			}
+		},
+
+		inline: {
+			dist: {
+				options: {
+					inlineTagAttributes: {
+						js: 'data-inlined="true"',
+						css: 'data-inlined="true"'
+					},
+					cssmin: true,
+					uglify: true
+				},
+				src:'src/index.html',
+				dest:'dist/index.html'
+			}
+		},
+
+		htmlmin: {
+
+			options: {
+				removeComments: true,
+				collapseWhitespace: true
+			},
+
+			dist: {
+				files: [
+					{
+						expand: true,
+						cwd: 'dist/',
+						src: ['*.html'],
+						dest: 'dist/'
+					}
+				]
+			}
+		},
+
+		concat: {
+			options: {
+				separator: ';',
+			},
+			dist: {
+				src: app_files,
+				dest: output
+			},
+			test: {
+				src: app_files,
+				dest: test_output
+			}
+		},
+
+		mochaTest: {
+			test: {
+				options: {
+					reporter: 'spec'
+				},
+				src: ['test/**/*.js']
+			}
+		}
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-mocha-test');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-htmlmin');
+	grunt.registerTask('default', ['copy','concat','htmlmin']);
+};
